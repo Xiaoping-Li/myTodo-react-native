c59dfbededb86721d95d05dc8eb52d8702d79a04
Xiaoping-Li <narci2008@gmail.com>
2019-01-30 14:24:53 -0800

    Modify style of my todo

--- myTodo/App.js
+++ myTodo/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import TodoList from './components/TodoList';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <Text>My Todos</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>My Todos</Text>
         <TodoList />
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 40,
+  },
+  title: {
+    fontSize: 40,
+    marginBottom: 40,
+    textAlign: 'center',
+  }
+});
